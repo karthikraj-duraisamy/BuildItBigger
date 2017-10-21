a1638433f9c2313fffc622d87e7d27709453fe2d
karthik <raj.karthik777@gmail.com>
2017-10-22 02:15:23 +0530

    Removing default comment

--- app/src/free/java/net/karthikraj/excercise/builditbigger/free/MainActivityFragment.java
+++ app/src/free/java/net/karthikraj/excercise/builditbigger/free/MainActivityFragment.java
@@ -16,11 +16,6 @@ import net.karthikraj.excercise.builditbigger.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link BlankFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link BlankFragment#newInstance} factory method to
- * create an instance of this fragment.
  */
 public class MainActivityFragment extends Fragment {
 
